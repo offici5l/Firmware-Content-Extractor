46b535ac839fa8dc00a0d5e80636f79ff2faa3e9
offici5l <127665988+offici5l@users.noreply.github.com>
2024-11-24 13:03:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function handleRequest(request) {
   const parts = requestBody.split(" ");
 
   if (parts.length < 2) {
-    return new Response("\nMissing parameters. \n\nUsage:\n curl -d \"<get> <url>\" <worker-url>\n\nExample:\n curl -d \"boot_img https://example.com/file.zip\" fce.offici5l.workers.dev\n\n", { status: 400 });
+    return new Response("\nMissing parameters!\n\nUsage: \ncurl -d <get> <url> <worker-url>\n\nExample:\n curl -d boot_img https://example.com/file.zip fce.offici5l.workers.dev\n", { status: 400 });
   }
 
   const get = parts[0];
@@ -63,5 +63,3 @@ async function handleRequest(request) {
     }
   }
 }
-
-addEventListener('fetch', event => event.respondWith(handleRequest(event.request)));
