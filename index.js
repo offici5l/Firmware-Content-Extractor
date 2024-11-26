9405113e209ff5038e20104ac26ef544e20e4d7f
offici5l <127665988+offici5l@users.noreply.github.com>
2024-11-26 11:01:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ export default {
       const finalUrlResponse = await fetch(finalUrl, { method: 'HEAD' });
       if (finalUrlResponse.ok) {
         return new Response(`\nresult: available\nlink: ${finalUrl}\n`, { status: 200 });
-      }
+      } 
 
       const track = Date.now().toString();
       const data = { ref: "main", inputs: { get, url, track } };
@@ -48,39 +48,7 @@ export default {
       });
 
       if (githubResponse.ok) {
-        async function checkJobStatus() {
-          const response = await fetch('https://api.github.com/repos/offici5l/Firmware-Content-Extractor/actions/workflows/FCE.yml/runs');
-          const data = await response.json();
-
-          for (const workflowRun of data.workflow_runs) {
-            const jobUrl = `${workflowRun.url}/jobs`;
-            const jobResponse = await fetch(jobUrl);
-            const jobData = await jobResponse.json();
-
-            for (const job of jobData.jobs) {
-              if (job.name === track) {
-                while (true) {
-                  const upload = jobData.jobs.find(step => step.name === "upload");
-                  const status = upload.status;
-
-                  if (status === "completed") {
-                    const conclusion = upload.conclusion;
-                    if (conclusion === "success") {
-                      return new Response(conclusion);
-                    } else {
-                      return new Response("failed");
-                    }
-                  }
-
-                  await new Promise(resolve => setTimeout(resolve, 10000));
-                }
-              }
-            }
-          }
-        }
-
-        const statusResponse = await checkJobStatus();
-        return statusResponse;
+        return new Response(`\nresult: It will be available\nlink: ${finalUrl}\n`, { status: 200 });
       } else {
         const githubResponseText = await githubResponse.text();
         return new Response(`GitHub Response Error: ${githubResponseText}`, { status: 500 });
